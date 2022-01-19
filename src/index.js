226dae745c6cb9ca859b75abd36a469ad6f510b5
Rebekah Zhou <rebekahzhou@gmail.com>
2022-01-19 14:20:31 -0800

    commit tree c7410b49f2fa47f0415f273abddadc51edfcdca2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const input = document.querySelector('#searchByID');
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(res => res.json())
+      .then(data => {
+        const title = document.querySelector('#movieDetails h4')
+        const summary = document.querySelector('#movieDetails p')
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
